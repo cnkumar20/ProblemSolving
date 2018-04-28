d6b898ddbf1c20972ff7007df3d8436d9da105be
nchandregowda <nchandregowda@conversantmedia.com>
2018-04-28 04:35:23 -0500

    Data structure

---
+++ data-structure/dict/simple_dictionary.py
@@ -0,0 +1,14 @@
+def fiddle(dict0):
+    print(dict0["lname"])
+    print(dict0.pop("address"))
+    print([x for x in dict0.keys()])
+    print(len(dict0))
+    
+
+if __name__=="__main__":
+    dict0 = dict()
+    dict0["name"]="Nandiesh"
+    dict0["lname"]="Chandregowda"
+    dict0["address"]="Chicago"
+
+    fiddle(dict0)
